0d4f5ba94da63fcad904eee623e810be9560c1c5
zwy <15133687879@163.com>
2024-02-14 17:18:28 +0800

    添加svm模型

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mtb",
-    version="3.1.11",
+    version="3.1.12",
     author="zwy",
     author_email="15133687879@163.com",
     packages=find_packages()
